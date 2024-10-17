7ff95f205a090e8bee74e97f54f2dd03cd0e0560
Nabil Az <nabil.azamoum@outlook.fr>
2024-10-17 15:33:03 +0200

    footer creation

---
+++ wp-content/themes/oceanwp-child/footer.php
@@ -0,0 +1,15 @@
+<footer id="site-footer" class="header-footer-group ">
+
+<?php
+            wp_nav_menu(array(
+                'theme_location' => 'footer-planty', // Nom de l'emplacement du menu
+                
+            ));
+            ?>
+              
+        </footer><!-- #site-footer -->
+
+      <?php wp_footer(); ?>
+
+   </body>
+</html>
